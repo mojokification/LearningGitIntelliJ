03d1d16a12161ffb22cdf11f100931fc8195ca1b
alorsondanse2023 <132344330+alorsondanse2023@users.noreply.github.com>
2023-05-02 14:36:42 +0530

    Update DevClass.java
    Added the dash in place of plus

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,7 +1,7 @@
 public class DevClass {
 
     public static void main(String[] args) {
-        System.out.println("dev Class + alors on danse");
+        System.out.println("dev Class - alors on danse");
         //checkin  from alorsondanse 
     }
 }
